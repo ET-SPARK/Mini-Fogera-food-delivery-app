e9ead20443363a8b5ba29257a4e8cb3e145468cb
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-07-26 12:39:39 +0300

    edited

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
 import AppNavigation from './src/AppNavigation'
-import * as Location from 'expo-location';
+
 const App = () => {
   return <AppNavigation />
 }
