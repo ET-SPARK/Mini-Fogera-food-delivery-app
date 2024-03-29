b9e475d476705e7cb42d8058fff8ddbcea0c471e
Samuel <samuelwoyesso2016@gmail.com>
2024-03-29 14:38:44 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,13 @@ import { getDatabase } from 'firebase/database';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCrNmmJvAXBQ_Eq8keN-RuPUwyn5hR4dwo",
-    authDomain: "spark-foods-a5293.firebaseapp.com",
-    projectId: "spark-foods-a5293",
-    storageBucket: "spark-foods-a5293.appspot.com",
-    messagingSenderId: "287080897584",
-    appId: "1:287080897584:web:7609db9133ceec5b1be1f5",
-    measurementId: "G-TRLZBT2HCP"
+    apiKey: "your apiKey"
+    authDomain: "*",
+    projectId: "*",
+    storageBucket: "*",
+    messagingSenderId: "*",
+    appId: "*",
+    measurementId: "*"
   };
 
 const app = initializeApp(firebaseConfig);
